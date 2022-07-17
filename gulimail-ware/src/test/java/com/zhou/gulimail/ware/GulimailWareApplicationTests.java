1b08bf192aca4b845805dcc5ca5d44998ae11faa
zhouhaizhan <1363003594@qq.com>
2022-07-17 13:26:10 +0800

    项目模块搭建

---
+++ gulimail-ware/src/test/java/com/zhou/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.gulimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
