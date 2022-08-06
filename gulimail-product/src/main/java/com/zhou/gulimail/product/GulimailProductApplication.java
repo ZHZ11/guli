f59e4089c2b65612b1306998d26d564cdb2f0979
zhouhaizhan <1363003594@qq.com>
2022-08-06 18:50:12 +0800

    gateway配置

--- gulimail-product/src/main/java/com/zhou/gulimail/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/zhou/gulimail/product/GulimailProductApplication.java
@@ -3,9 +3,11 @@ package com.zhou.gulimail.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.zhou.gulimail.product.dao")
+@EnableDiscoveryClient
 public class GulimailProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimailProductApplication.class, args);
--- gulimail-ware/src/main/java/com/zhou/gulimail/ware/GulimailWareApplication.java
+++ gulimail-ware/src/main/java/com/zhou/gulimail/ware/GulimailWareApplication.java
@@ -3,9 +3,11 @@ package com.zhou.gulimail.ware;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.zhou.gulimail.ware.dao")
+@EnableDiscoveryClient
 public class GulimailWareApplication {
 
     public static void main(String[] args) {
